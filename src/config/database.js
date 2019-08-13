fc2c973aa3eebdb8db64d1ac5bd0340dde9c6cc9
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-08-13 08:46:10 -0300

    sequelize configuration

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'pg',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'pedemanga',
+  database: 'go_barber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
