3586add2d09c413c725773cb435aa6673af62b22
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-08-14 13:03:56 -0300

    adding pagination

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -63,9 +63,14 @@ class AppointmentController {
   }
 
   async index(req, res) {
+    const { page = 1 } = req.query;
+
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
+      attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
       include: [
         {
           model: User,
@@ -76,7 +81,6 @@ class AppointmentController {
           ],
         },
       ],
-      attributes: ['id', 'date'],
     });
 
     return res.status(200).send(appointments);
