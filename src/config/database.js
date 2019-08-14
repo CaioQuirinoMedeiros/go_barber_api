43b4a396bec6445d30cd40d5eefb04773f4d549e
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-08-14 13:05:07 -0300

    configuration in environment variables

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
-  dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'pedemanga',
-  database: 'go_barber',
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   define: {
     timestamps: true,
     underscored: true,
