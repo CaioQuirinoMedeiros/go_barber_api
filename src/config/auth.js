bc0037d18def8dcfa0ec0331e783c779e3bd8189
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-08-15 15:13:02 -0300

    .env improvements

--- src/config/auth.js
+++ src/config/auth.js
@@ -3,6 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  secret: process.env.JWT_SECRET,
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
--- src/config/mail.js
+++ src/config/mail.js
@@ -1,5 +1,5 @@
 export default {
-  apiKey: process.env.MAIL_KEY,
+  apiKey: process.env.SG_KEY,
   default: {
     from: 'caio.quirino.medeiros@gmail.com',
   },
