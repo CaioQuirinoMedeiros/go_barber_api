ba146dd14a6bf52d191a5d799f2d2be401bd79c7
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2020-01-03 23:37:13 -0300

    fixing formatted date on getDateAsString

--- src/app/helpers/date.js
+++ src/app/helpers/date.js
@@ -3,7 +3,4 @@ import pt from 'date-fns/locale/pt';
 
 export const getAppointmentDate = date => startOfHour(parseISO(date));
 
-export const getDateAsString = date =>
-  format(date, 'd [de] MMMM[, às] H:mm[h]', {
-    locale: pt,
-  });
+export const getDateAsString = date => format(date, 'Pp', { locale: pt });
