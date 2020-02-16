2d9a19627f7c1f125508096cf0e9a38d511547ca
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2020-02-16 04:08:38 -0300

    Fixing dates

--- src/app/services/AvailableService.js
+++ src/app/services/AvailableService.js
@@ -24,17 +24,27 @@ class AvailableService {
     })
 
     const schedule = [
-      '08:00',
-      '09:00',
-      '10:00',
-      '11:00',
+      '11:30',
       '12:00',
+      '12:30',
       '13:00',
+      '13:30',
       '14:00',
+      '14:30',
       '15:00',
+      '15:30',
       '16:00',
+      '16:30',
       '17:00',
+      '17:30',
       '18:00',
+      '18:30',
+      '19:00',
+      '19:30',
+      '20:00',
+      '20:30',
+      '21:00',
+      '21:30',
     ]
 
     const available = schedule.map(time => {
@@ -44,7 +54,7 @@ class AvailableService {
 
       return {
         time,
-        value: value.toISOString(),
+        value: `${format(date, 'yyyy-MM-dd')}T${time}:00.000Z`,
         available:
           isAfter(value, new Date()) &&
           !appointments.find(
