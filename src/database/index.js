e5b8cabdf215303984ad044fa6c481a97a1674c7
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2020-02-16 03:35:34 -0300

    showing message on databse connect

--- src/database/index.js
+++ src/database/index.js
@@ -1,35 +1,42 @@
-import Sequelize from 'sequelize';
-import mongoose from 'mongoose';
+import Sequelize from 'sequelize'
+import mongoose from 'mongoose'
 
-import databaseConfig from '../config/database';
+import databaseConfig from '../config/database'
 
-import User from '../app/models/User';
-import File from '../app/models/File';
-import Appointment from '../app/models/Appointment';
+import User from '../app/models/User'
+import File from '../app/models/File'
+import Appointment from '../app/models/Appointment'
 
-const models = [User, File, Appointment];
+const models = [User, File, Appointment]
 
 class Database {
   constructor() {
-    this.init();
-    this.mongo();
+    this.init()
+    this.mongo()
   }
 
   init() {
-    this.connection = new Sequelize(databaseConfig);
+    this.connection = new Sequelize(databaseConfig)
 
-    models.map(model => model.init(this.connection));
+    models.map(model => model.init(this.connection))
     models.map(
       model => model.associate && model.associate(this.connection.models)
-    );
+    )
+
+    this.connection
+      .authenticate()
+      .then(() =>
+        console.log('Database connection has been established successfully.')
+      )
+      .catch(err => console.error('Unable to connect to the database:', err))
   }
 
   mongo() {
     this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useFindAndModify: true,
-    });
+    })
   }
 }
 
-export default new Database();
+export default new Database()
